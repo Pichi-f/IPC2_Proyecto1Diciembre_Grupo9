ff761955ecf0596076c2704d370872f98f26721f
Luis Pichiyá <3502658870401@ingenieria.usac.edu.gt>
2023-12-15 09:27:44 -0600

    Interfaz listas reproducción

--- v1/v1/procesador.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as ET
-from lista_doble import *
-from multimedia import *
-
-
-class Procesador:
-    def __init__(self) -> None:
-        pass
--- v1/v2/multimedia.py
+++ v1/v2/multimedia.py
@@ -9,11 +9,18 @@ class Cancion:
 
 
 class Playlist:
-    def __init__(self, lista, cancion, artista, album, imagen, ruta):
+    def __init__(self, lista, reproduccion, cancion, artista, album, imagen, ruta):
         self.lista = lista
+        self.reproduccion = reproduccion
         self.cancion = cancion
         self.artista = artista
         self.album = album
         self.imagen = imagen
         self.ruta = ruta
+        self.primer_dato = None
+
+
+class ListaLista:
+    def __init__(self, lista):
+        self.lista = lista
         self.primer_dato = None
